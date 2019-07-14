982dfe7a8a0fd1ede7cd9f6473c262f7d0d1539a
Moritz Bruder <muesli4@gmail.com>
2019-07-14 16:16:44 +0200

    Add fuzzy directory matching when searching covers
    It's only fuzzy in the sense that it will drop up to the first two path
    segments of the path given by MPD.

--- src/util.cpp
+++ src/util.cpp
@@ -2,7 +2,6 @@
 
 #include <algorithm>
 
-
 #include <boost/filesystem.hpp>
 
 std::string_view basename(std::string_view const path)
@@ -37,17 +36,39 @@ std::string string_from_ptr(char const * const ptr)
     return ptr == 0 ? "" : ptr;
 }
 
+std::string_view remove_first_directory(std::string_view path)
+{
+    std::size_t pos = path.find(PATH_SEP);
+    return pos != std::string::npos ? path.substr(pos + 1) : path;
+}
+
 std::optional<std::string> find_cover_file(std::string_view const rel_song_dir_path, std::string_view const base_dir, std::vector<std::string> names, std::vector<std::string> extensions)
 {
-    std::string const abs_cover_dir = absolute_cover_path(base_dir, basename(rel_song_dir_path));
-    for (auto const & name : names)
+    // Try to guess a fuzzy location if the one given by MPD doesn't work
+    // (i.e. up to two times remove the directory in front).
+    //
+    // E.g., if on the host the songs are stored in an auto-mounted directory
+    // but not on the client. Assume there is a mirror that is not auto-mounted.
+    //
+    // TODO Allow functionality to be tuned off.
+    std::string_view p = remove_first_directory(rel_song_dir_path);
+    std::string_view custom_rel_song_dir_paths[] =
+        { rel_song_dir_path
+        , p
+        , remove_first_directory(p)
+        };
+    for (auto const & custom_rel_song_dir_path : custom_rel_song_dir_paths)
     {
-        for (auto const & ext : extensions)
+        std::string const abs_cover_dir = absolute_cover_path(base_dir, basename(custom_rel_song_dir_path));
+        for (auto const & name : names)
         {
-            std::string const cover_path = abs_cover_dir + '/' + name + "." + ext;
-            if (boost::filesystem::exists(boost::filesystem::path(cover_path)))
+            for (auto const & ext : extensions)
             {
-                return cover_path;
+                std::string const cover_path = abs_cover_dir + '/' + name + "." + ext;
+                if (boost::filesystem::exists(boost::filesystem::path(cover_path)))
+                {
+                    return cover_path;
+                }
             }
         }
     }
